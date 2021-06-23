1cde2e33d8dc80bd446654f86f610368cb34933e
Shogo Ohta <shogo@xcoo.jp>
2021-06-23 11:19:20 +0900

    first commit

---
+++ src/chrovis/duxhund.clj
@@ -0,0 +1,6 @@
+(ns chrovis.duxhund)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/chrovis/duxhund_test.clj
@@ -0,0 +1,7 @@
+(ns chrovis.duxhund-test
+  (:require [clojure.test :refer :all]
+            [chrovis.duxhund :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
