5e2b723d7ea4260f53c4350afffb76c71cb4d78b
Shogo Ohta <shogo@xcoo.jp>
2021-08-24 17:51:22 +0900

    Add CLI

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]
+                 [clj-sub-command "0.6.0"]
                  [cljam "0.8.0"]]
   :repl-options {:init-ns duxhund.core}
   :main duxhund.cli
