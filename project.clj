6959dc28580cecbfee01fcf1caa5b2617a43dc29
Shogo Ohta <shogo@xcoo.jp>
2022-08-03 11:39:29 +0900

    Update cljam to 0.8.3

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "https://www.gnu.org/licenses/gpl-3.0.html"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [clj-sub-command "0.6.0"]
-                 [cljam "0.8.2"]]
+                 [cljam "0.8.3"]]
   :repl-options {:init-ns duxhund.core}
   :main duxhund.cli
   :profiles {:dev
