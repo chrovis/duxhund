89f656b99c86998db0fc168079d829178492e965
Shogo Ohta <shogo@xcoo.jp>
2022-08-03 11:38:34 +0900

    Update Clojure to 1.11.1

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/chrovis/duxhund"
   :license {:name "GPL-3.0-or-later"
             :url "https://www.gnu.org/licenses/gpl-3.0.html"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [clj-sub-command "0.6.0"]
                  [cljam "0.8.2"]]
   :repl-options {:init-ns duxhund.core}
